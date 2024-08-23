c5e03e1cc3a90e86fc365094034448c83a0463aa
garedunord21 <garedunord21@gmail.com>
2024-08-23 16:08:43 +0900

    2024.08.23 15:50

--- src/main/java/com/pjh/springbasic/controller/ResponseDataController.java
+++ src/main/java/com/pjh/springbasic/controller/ResponseDataController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,12 +33,14 @@ public class ResponseDataController {
     // - 반환타입으로 ResponseEntity 타입으로 지정
     //   ResponseEntity 클래스는 제너릭으로 response body 데이터의 타입을 전달해야함
     @GetMapping("/response-entity/{name}")
-    public void responseEntity(
+    public ResponseEntity<ResponseDto> responseEntity(
         @PathVariable("name") String name
     ) {
-
+        ResponseDto response = new ResponseDto(name, new Date());
+        // return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
-
+    
 
 }
 
