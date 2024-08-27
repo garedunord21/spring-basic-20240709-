d2d1ed1251650b751f3ada40f28b55a7cf6858b0
garedunord21 <garedunord21@gmail.com>
2024-08-27 17:00:17 +0900

    2024.08.27 16:50

--- jpa_database.sql
+++ jpa_database.sql
@@ -6,6 +6,10 @@ CREATE TABLE sample_table_1 (
     sample_column INT NOT NULL
 );
 
+INSERT INTO sample_table_1 VALUES ('홍길동', 1);
+
+SELECT * FROM sample_table_1;
+
 CREATE TABLE sample_table_2 (
 	sample_ai INT PRIMARY KEY AUTO_INCREMENT,
     sample_column BOOLEAN
