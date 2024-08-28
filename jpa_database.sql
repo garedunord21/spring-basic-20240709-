ba897c8272a81d9dd32b9a47f2becb9ca475f94d
garedunord21 <garedunord21@gmail.com>
2024-08-28 16:13:49 +0900

    2024.08.28 15:50

--- jpa_database.sql
+++ jpa_database.sql
@@ -14,3 +14,29 @@ CREATE TABLE sample_table_2 (
 	sample_ai INT PRIMARY KEY AUTO_INCREMENT,
     sample_column BOOLEAN
 );
+
+CREATE TABLE sample_user (
+	user_id VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    address TEXT,
+    tel_number VARCHAR(20) NOT NULL UNIQUE
+);
+
+INSERT INTO sample_user VALUES ('qwer1234', 'qwer1234', '홍길동', '부산광역시', '010-1111-1111');
+INSERT INTO sample_user VALUES ('asdf1234', 'qwer1234', '김철수', '울산광역시', '010-1111-2222');
+INSERT INTO sample_user VALUES ('zxcv1234', 'qwer1234', '이영희', '부산광역시', '010-1111-3333');
+INSERT INTO sample_user VALUES ('poiu4321', 'qwer1234', '홍길동', '서울특별시', '010-1111-4444');
+
+SELECT * FROM sample_user;
+
+SELECT * FROM sample_user WHERE name = "김철수";
+
+SELECT * FROM sample_user WHERE tel_number = ?;
+
+SELECT * FROM sample_user WHERE name = ? AND address = ?; 
+
+SELECT * FROM sample_user WHERE address LIKE '%부산%';
+
+SELECT * FROM sample_user WHERE name = ? ORDER BY tel_number DESC;
+
---
+++ src/main/java/com/pjh/springbasic/repository/SampleUserRepository.java
@@ -0,0 +1,36 @@
+package com.pjh.springbasic.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pjh.springbasic.entity.SampleUserEntity;
+
+@Repository
+public interface SampleUserRepository
+extends JpaRepository<SampleUserEntity, String> {
+
+    // Query Method:
+    // - Repository의 메서드 선언시에 지정된 패턴에 따라 메서드명을 작성하면 JPA가 SQL을 만들어주는 방법
+    // - findBy : 필드명을 기준으로 모든 컬럼을 조회할 때 사용, findBy 뒤에 필드명을 붙여서 작성, 필드명의 첫글자는 대문자이어야함
+    List<SampleUserEntity> findByName(String name);
+    SampleUserEntity findByTelNumber(String telNumber);
+
+    // - And/Or : and 연산 혹은 or 연산에 사용됨, 필드와 필드 사이에 사용
+    //   And/Or의 우선순위 주의
+    List<SampleUserEntity> findByNameAndAddress(String name, String address);
+
+    // - Like, NotLike, StartingWith, EndingWith, Containing: Like 연산에 대하여 사용, 필드 뒤에 사용
+    List<SampleUserEntity> findByAddressContaining(String address);
+
+    // - OrderBy: 정렬을 사용할 필드를 지정하여 Desc, Asc 지정
+    List<SampleUserEntity> findByNameOrderByTelNumberDesc(String name);
+
+    // - existsBy: 조건에 해당하는 레코드가 존재하는지 여부 확인 시 사용
+    boolean existsByName(String name);
+
+    // - countBy: 조건에 해당하는 레코드의 개수 확인 시 사용
+    int countByName(String name);
+
+}
