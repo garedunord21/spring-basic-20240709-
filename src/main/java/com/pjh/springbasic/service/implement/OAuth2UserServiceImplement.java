76a3b852318d6f35cb33a2933b1c8a9ab3beb149
garedunord21 <garedunord21@gmail.com>
2024-09-05 16:51:09 +0900

    2024.09.05 16:50

---
+++ src/main/java/com/pjh/springbasic/service/implement/OAuth2UserServiceImplement.java
@@ -0,0 +1,34 @@
+package com.pjh.springbasic.service.implement;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class OAuth2UserServiceImplement extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registration = userRequest.getClientRegistration().getClientName();
+    
+        try {
+            System.out.println(registration);
+            System.out.println("=========================================");
+            System.out.println(new ObjectMapper().writeValueAsString(oAuth2User.getAttributes()));
+            System.out.println(oAuth2User.getName());
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+    
+        return oAuth2User;
+    }
+
+    
+
+}
--- src/main/java/com/pjh/springbasic/service/object/CustomOAuth2User.java
+++ src/main/java/com/pjh/springbasic/service/object/CustomOAuth2User.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
-import org.springframework.beans.propertyeditors.CustomNumberEditor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
